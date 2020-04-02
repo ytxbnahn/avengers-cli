6a8b21e3daafcd06fa6bbf86d9bdb86db2d7c11d
ytmty <ytnsmty@gmail.com>
2020-04-02 10:06:41 +0800

    init project

---
+++ src/main.js
@@ -0,0 +1,72 @@
+import program from "commander";
+import { VERSION } from "./utils/constants";
+import apply from "./index";
+import chalk from "chalk";
+
+/**
+ * avengers commands
+ *    - config
+ *    - init
+ */
+
+let actionMap = {
+  init: {
+    description: "generate a new project from a template",
+    usages: ["avengers init templateName projectName"]
+  },
+  config: {
+    alias: "cfg",
+    description: "config .avengersrc",
+    usages: [
+      "avengers config set <k> <v>",
+      "avengers config get <k>",
+      "avengers config remove <k>"
+    ]
+  }
+  //other commands
+};
+
+// 添加 init / config 命令
+Object.keys(actionMap).forEach(action => {
+  program
+    .command(action)
+    .description(actionMap[action].description)
+    .alias(actionMap[action].alias) //别名
+    .action(() => {
+      switch (action) {
+        case "config":
+          //配置
+          apply(action, ...process.argv.slice(3));
+          break;
+        case "init":
+          apply(action, ...process.argv.slice(3));
+          break;
+        default:
+          break;
+      }
+    });
+});
+
+function help() {
+  console.log("\r\nUsage:");
+  Object.keys(actionMap).forEach(action => {
+    actionMap[action].usages.forEach(usage => {
+      console.log("  - " + usage);
+    });
+  });
+  console.log("\r");
+}
+program.usage("<command> [options]");
+// avengers -h
+program.on("-h", help);
+program.on("--help", help);
+// avengers -V   VERSION 为 package.json 中的版本号
+program.version(VERSION, "-V --version").parse(process.argv);
+
+// avengers 不带参数时
+if (!process.argv.slice(2).length) {
+  program.outputHelp(make_green);
+}
+function make_green(txt) {
+  return chalk.green(txt);
+}
